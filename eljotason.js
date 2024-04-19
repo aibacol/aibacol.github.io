32e1ca1c5020895819c609f0cd8a48e8c9e7fee6
AibAcol <88537346+aibacol@users.noreply.github.com>
2024-04-19 14:09:09 +0200

    Add files via upload

---
+++ eljotason.js
@@ -0,0 +1,11 @@
+var val="Projecte Petjades té com a finalitat inserir al col·lectiu amb situació de drogodependència a l'àmbit social i laboral, deixant de costat els prejudicis del col·lectiu perquè puguen desenvolupar-se a la societat sense discriminacions."
+var cas="Projecte Petjades tiene como finalidad insertar al colectivo en situación de drogodependencia en el ámbito laboral, sin importar los prejuicios del colectivo y así poder desarrollarse en sociedad sin discriminaciones."
+
+function traduval(){
+    document.getElementById("text-video").textContent=val;
+    console.log("Traduït")
+}
+function traducas(){
+    document.getElementById("text-video").textContent=cas;
+    console.log("Traducido")
+}
