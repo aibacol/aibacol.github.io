e0f72ddc74cb814ad47516bd55fcbecc941665c1
AibAcol <aibacol@teleco.upv.es>
2024-04-21 14:10:12 +0200

    2nd round

--- eljotason.js
+++ eljotason.js
@@ -1,5 +1,11 @@
-var val="Projecte Petjades té com a finalitat inserir al col·lectiu amb situació de drogodependència a l'àmbit social i laboral, deixant de costat els prejudicis del col·lectiu perquè puguen desenvolupar-se a la societat sense discriminacions."
-var cas="Projecte Petjades tiene como finalidad insertar al colectivo en situación de drogodependencia en el ámbito laboral, sin importar los prejuicios del colectivo y así poder desarrollarse en sociedad sin discriminaciones."
+var val="Projecte Petjades té com a finalitat inserir al col·lectiu amb situació de drogodependència a l'àmbit social i laboral,\
+ deixant de costat els prejudicis del col·lectiu perquè puguen desenvolupar-se a la societat sense discriminacions. Fem emprar\
+ unes eines com: la entrevista personal, el IPI, entrenaments de habilitats socials, entre altres.";
+
+var cas="Projecte Petjades tiene como finalidad insertar al colectivo en situación de drogodependencia en el ámbito laboral, \
+sin importar los prejuicios del colectivo y así poder desarrollarse en sociedad sin discriminaciones. Usamos herramientas como: \
+la entrevista personal, el IPI, entrenamientos de habilidades sociales, entre otros.";
+
 
 function traduval(){
     document.getElementById("text-video").textContent=val;
